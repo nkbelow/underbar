c1d83756b63051d6defe84bf0cd4eb388a2ad2a5
NicholasBelow <nkbelow@gmail.com>
2017-01-29 16:25:57 -0800

    Fix _.invoke and _.sortby functions so that tests pass

--- src/underbar.js
+++ src/underbar.js
@@ -407,6 +407,16 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(element) {
+      var func;
+      if (typeof functionOrKey === "string") {
+        func = element[functionOrKey]
+      }
+      else {
+        func = functionOrKey;
+      }
+      return func.apply(element, args);
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
@@ -414,6 +424,16 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    if (typeof iterator === "function") {
+      return collection.sort(function(first, next) {
+        return iterator(first) - iterator(next);
+      });
+    }
+    else {
+      return collection.sort(function(first, next) {
+        return first[iterator] - next[iterator];
+      });
+    }
   };
 
   // Zip together two or more arrays with elements of the same index
